46a24165fc3b16ea1c89f1e13f8c5f92b3fb7254
plopezrodrigo <patrylr84@gmail.com>
2022-10-24 15:02:33 +0000

    Mi primer commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.querySelector("#btn").addEventListener("click", () => {
+  document.querySelector("#name").innerHTML = domainName();
+});
+
+let pronoun = ["the", "our"];
+let adj = ["greate", "big"];
+let noun = ["jogger", "racoon"];
+
+function domainName() {
+  let i = 0;
+  let j = 0;
+  let k = 0;
+
+  let combination1 = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        combination1 += pronoun[i] + adj[j] + noun[k] + ".com <br>";
+      }
+    }
+  }
+  return combination1;
+}
